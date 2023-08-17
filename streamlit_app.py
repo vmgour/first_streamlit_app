eae0ff710f99a9564fce11573776f9f799e97d35
vmgour <62712677+vmgour@users.noreply.github.com>
2023-08-17 12:11:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,28 @@
-import streamlit;
-streamlit.title('First Streamlit App I am developing');
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import streamlit 
+import snowflake.connector 
+import pandas streamlit.title('Zena\'s Amazing Athleisure Catalog') 
+# connect to snowflake 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
+my_cur = my_cnx.cursor() 
+# run a snowflake query and put it all in a var called my_catalog 
+my_cur.execute("select color_or_style from catalog_for_website") 
+my_catalog = my_cur.fetchall() 
+# put the dafta into a dataframe 
+df = pandas.DataFrame(my_catalog) 
+# temp write the dataframe to the page so I Can see what I am working with # 
+streamlit.write(df) 
+# put the first column into a list 
+color_list = df[0].values.tolist() 
+print(color_list) 
+# Let's put a pick list here so they can pick the color 
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list)) 
+# We'll build the image caption now, since we can 
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!' 
+# use the option selected to go back and get all the info from the database
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
-
-import snowflake.connector
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';") 
+df2 = my_cur.fetchone() 
+streamlit.image( df2[0], width=400, caption= product_caption ) 
+streamlit.write('Price: ', df2[1]) 
+streamlit.write('Sizes Available: ',df2[2]) 
+streamlit.write(df2[3])
